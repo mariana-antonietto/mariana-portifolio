2ea8cb04474d57fb1ef72ddec5699c9026b08e90
Alexandre Morales Suete <alexandre.suete@contmatic.com.br>
2022-08-04 21:52:54 -0300

    atualização do portifólio

--- src/app/components/pag inicial/pag-inicial.component.ts
+++ src/app/components/pag inicial/pag-inicial.component.ts
@@ -8,5 +8,13 @@ import { Component } from "@angular/core";
   }
 )
 export class PagInicial{
-
+public goToGithub(){
+  window.open("https://github.com/mariana-antonietto",'_blank')
+}
+public goToLinkedin(){
+  window.open("https://www.linkedin.com/in/mariana-antonietto/", '_blank')
+}
+public goToCurriculo(){
+  window.open("https://drive.google.com/file/d/1IobS8jXXRQCMzqYKcnA5RFnYh2Zr9cfB/view?usp=drivesdk", '_blank')
+}
 }
